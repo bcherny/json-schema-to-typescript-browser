aba5ea83bc43e2127b19744e9289572776605123
Boris Cherny <boris@performancejs.com>
2017-04-22 18:39:19 -0700

    :sunrise:

---
+++ README.md
@@ -0,0 +1,11 @@
+# json-schema-to-typescript-browser
+
+> Browser usage demo for json-schema-to-typescript
+
+## Usage
+
+```sh
+npm install
+npm start
+# open http://localhost:8080 in a web browser
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+const leftInput = document.querySelector('#leftInput textarea')
+const rightOutput = document.querySelector('#rightOutput textarea')
+
+leftInput.addEventListener('input', compile)
+
+// initial compile
+compile()
+
+function compile() {
+  const json = eval('(' + leftInput.value + ')')
+  console.log('json', json)
+  window.jstt.compile(json, 'Demo')
+    .then(ts => {
+      rightOutput.value = ts
+    })
+    .catch(e => {
+      console.error(e)
+    })
+}
