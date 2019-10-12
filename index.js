eafc7e5c618af74337bed29918695d59b5b9f963
Gareth Jones <jones258@gmail.com>
2019-10-13 12:24:40 +1300

    Apply `prettier` formatting to project

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const leftInput = document.querySelector('#leftInput textarea')
-const rightOutput = document.querySelector('#rightOutput textarea')
+const leftInput = document.querySelector('#leftInput textarea');
+const rightOutput = document.querySelector('#rightOutput textarea');
 
-leftInput.addEventListener('input', compile)
+leftInput.addEventListener('input', compile);
 
 // initial compile
-compile()
+compile();
 
 function compile() {
-  const json = eval('(' + leftInput.value + ')')
-  window.jstt.compile(json, 'Demo')
+  const json = eval('(' + leftInput.value + ')');
+  window.jstt
+    .compile(json, 'Demo')
     .then(ts => {
-      rightOutput.value = ts
+      rightOutput.value = ts;
     })
     .catch(e => {
-      console.error(e)
-    })
+      console.error(e);
+    });
 }
