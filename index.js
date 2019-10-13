c24e620573651fe4a48af2f66bdd33fd54e2dbe6
Gareth Jones <jones258@gmail.com>
2019-10-13 20:48:07 +1300

    Persist json in localStorage

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const rightOutput = document.querySelector('#rightOutput textarea');
 
 leftInput.addEventListener('input', compile);
 
+const localStorageKey = 'json-schema-to-typescript';
+const content = localStorage.getItem(localStorageKey);
+
+if (content) {
+  leftInput.value = content;
+}
+
 const options = {
   // bannerComment: '',
   declareExternallyReferenced: true,
@@ -36,6 +43,7 @@ Object.keys(options).forEach(option => {
 compile();
 
 function compile() {
+  localStorage.setItem(localStorageKey, leftInput.value);
   const json = eval('(' + leftInput.value + ')');
   window.jstt
     .compile(json, 'Demo', options)
