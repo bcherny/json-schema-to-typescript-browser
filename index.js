4f187cb29d044f7488165a16976e05c7145a7c8b
Gareth Jones <Jones258@Gmail.com>
2019-12-01 17:44:57 +1300

    Spring cleaning (#1)
    * Use `unpkg` for `json-schema-to-typescript` dependency instead of `node_modules`
    
    * Update `json-schema-to-typescript` to `7.1.0`
    
    * Install & setup `prettier` package
    
    * Apply `prettier` formatting to project
    
    * Sort `package.json`
    
    * Update `node-static` to `^0.7.11` (from `^0.7.10`)
    
    * Update `eslint` to `^6.5.1` (from `^4.10.0`)
    
    * Add `.editorconfig`
    
    * Install and configure `stylelint` package
    
    * Refactor css to use flex box
    
    * Use `Object.assign` instead of spread to support Edge
    
    * Adjust placement of closing `textarea` element to eliminate whitespace
    
    * Add support for configuration options via dropdown
    
    * Persist json in localStorage
    
    * Use template string literal instead of `+`
    
    * Short hand arrow functions
    
    * Implement error icon to show when compiling fails
    
    * Remove unused comments
    
    * Add friendly message to console for advance users

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-const leftInput = document.querySelector('#leftInput textarea')
-const rightOutput = document.querySelector('#rightOutput textarea')
+const leftInput = document.querySelector('#leftInput textarea');
+const rightOutput = document.querySelector('#rightOutput textarea');
+const errorIcon = document.getElementById('errorIcon');
 
-leftInput.addEventListener('input', compile)
+leftInput.addEventListener('input', compile);
+
+const localStorageKey = 'json-schema-to-typescript';
+const content = localStorage.getItem(localStorageKey);
+
+if (content) {
+  leftInput.value = content;
+}
+
+const options = {
+  declareExternallyReferenced: true,
+  enableConstEnums: true,
+  unreachableDefinitions: false,
+  strictIndexSignatures: false
+};
+
+// expose options for advance users
+window.options = options;
+
+console.info(
+  "Welcome! If you'd like to play around with more advance options,",
+  "you can mutate the 'options' object assigned to window :)"
+);
+
+Object.keys(options).forEach(option => {
+  const optionCheckbox = document.getElementById(option);
+
+  if (!(optionCheckbox instanceof HTMLInputElement)) {
+    console.warn(`"${option}" is missing an config element`);
+
+    return;
+  }
+
+  options[option] = optionCheckbox.checked;
+  optionCheckbox.addEventListener('change', () => {
+    options[option] = optionCheckbox.checked;
+    compile();
+  });
+});
 
 // initial compile
-compile()
+compile();
 
 function compile() {
-  const json = eval('(' + leftInput.value + ')')
-  window.jstt.compile(json, 'Demo')
-    .then(ts => {
-      rightOutput.value = ts
-    })
+  localStorage.setItem(localStorageKey, leftInput.value);
+
+  Promise.resolve()
+    .then(() => eval(`(${leftInput.value})`))
+    .then(json => window.jstt.compile(json, 'Demo', options))
+    .then(ts => (rightOutput.value = ts))
+    .then(() => (errorIcon.style.display = 'none'))
     .catch(e => {
-      console.error(e)
-    })
+      console.error(e);
+
+      errorIcon.style.display = null;
+    });
 }
